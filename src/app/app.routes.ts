0cf734a1c86d7c1dcf8584e7fc6feb137882cc4b
GLITCHGOD <omkardeshmane832@gmail.com>
2026-05-31 10:55:06 +0530

    make dashboard ui and sidebar and navbar changes and add some routs in backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { Users } from './users/users/users';
 import { AddMenuItem } from './menu/add-menu-item/add-menu-item';
+import { AddUser } from './users/add-user/add-user';
+import { EditUser } from './users/edit-user/edit-user';
+import { MenuItems } from './menu/menu-items/menu-items';
+import { Sidebar } from './layout/sidebar/sidebar';
+import { Dashboard } from './dashboard/dashboard/dashboard';
 
 
 export const routes: Routes = [
@@ -9,8 +14,29 @@ export const routes: Routes = [
         component : Users
     },
     {
+        path : 'adduser',
+        component : AddUser
+
+    },
+    {
+        path: 'edituser',
+        component : EditUser
+    },
+
+    {
         path : 'AddMenuItem',
         component : AddMenuItem
+    },
+    {path : 'MenuItems',
+        component : MenuItems
+    },
+    {
+        path : 'sidebar',
+        component : Sidebar
+    },
+     {
+        path : 'dashboard',
+        component : Dashboard
     },
     {
         path : '**',
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { AddMenuItem } from "./menu/add-menu-item/add-menu-item";
+import { Sidebar } from './layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink, AddMenuItem, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
